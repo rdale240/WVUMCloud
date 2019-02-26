f325a717098580e44e5dea5bae88e1b566b57b35
rdale240 <rmd127@miami.edu>
2019-02-26 15:06:27 -0500

    updates

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,26 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
- exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });
+exports.getDJ = functions.https.onRequest((request, response) => {
+    admin.initializeApp();
+    var db = admin.database().ref('/');
+    db.once('value').then(snap => {
+        console.log(snap.val())
+        response.send(snap.val());
+    })
+});
+
+exports.setDJ = functions.https.onRequest((request, response) => {
+    admin.initializeApp();
+    console.log(request);
+    res = { "status": "updated DJ" };
+    var db = admin.database().ref('/');
+    db.update({
+        currentDJ: "Charles"
+    }).then(() => {
+        response.send(res)
+    })
+});
